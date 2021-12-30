f661b9644817d58b9b5a4a9d27ff066d2b4d20d7
BoiMat <85798501+BoiMat@users.noreply.github.com>
2021-12-30 11:06:19 +0100

    Add files via upload

---
+++ Assignment1/section1/README.md
@@ -0,0 +1,29 @@
+# Assigment 1 FHPC course 2021/2022
+
+
+## Section  1: MPI programming
+
+Here all the commands to run the programs are listed. It is assumed that the programmer is inside an ORFEO computational node while running this commands.
+
+### Ring
+
+First thing is to load the essential modules for MPI programming:
+
+` module load openmpi-4.1.1+gnu-9.3.0 `
+
+Then the codes can be compiled.
+
+Odd - even processes approach:
+` mpicc ring_Odd-Even.c -o ring_Odd-Even.x `
+
+Non-blocking approaches:
+` mpicc ring_Send-Recv.c -o ring_Send-Recv.x `
+` mpicc ring_Isend-Irecv.c -o ring_Isend-Irecv.x `
+` mpicc ring_Isend-Recv.c -o ring_Isend-Recv.x `
+
+Then they can be run (here with 4 processes):
+
+` mpirun -np 4 ./ring_Odd-Even.x `
+` mpirun -np 4 ./ring_Send-Recv.x `
+` mpirun -np 4 ./ ring_Isend-Irecv.x `
+` mpirun -np 4 ./ring_Isend-Recv.x `
---
+++ Assignment1/section1/Ring/ring_Isend-Recv.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[])
+{
+    double start, end;
+    int rank, value, size, itag_r, itag_l, msgleft, msgright, tmpleft, tmpright, false=0, true=1;
+    int right_p, left_p;
+    int n_iter = 1000000;
+    MPI_Comm   ring_comm;
+
+    MPI_Init( &argc, &argv );
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+
+    MPI_Cart_create( MPI_COMM_WORLD, 1, &size, &true, 1, &ring_comm );
+    MPI_Cart_shift( ring_comm, 0, 1, &left_p, &right_p );
+    MPI_Comm_rank( ring_comm, &rank );
+    MPI_Comm_size( ring_comm, &size );
+
+    MPI_Barrier(ring_comm);
+    start = MPI_Wtime();
+	
+    MPI_Status stats[2];
+    MPI_Request reqs[2];
+    
+    for (size_t i=0; i<n_iter; i++) {
+	    
+	    msgleft = rank;
+	    msgright = -rank;
+	    
+	    itag_r = rank*10;
+	    itag_l = rank*10;
+	    
+	    do {
+		    MPI_Isend( &msgleft, 1, MPI_INT, left_p, itag_l, ring_comm, &reqs[0] );
+		    MPI_Isend( &msgright, 1, MPI_INT, right_p, itag_r, ring_comm, &reqs[1] );
+		    
+		    MPI_Recv( &tmpleft, 1, MPI_INT, right_p, MPI_ANY_TAG, ring_comm, &stats[0]);
+		    MPI_Recv( &tmpright, 1, MPI_INT, left_p, MPI_ANY_TAG, ring_comm, &stats[1]);
+		    
+		    itag_l = stats[0].MPI_TAG;
+		    itag_r = stats[1].MPI_TAG;
+		    
+		    MPI_Wait(&reqs[0], &stats[0]);
+		    MPI_Wait(&reqs[1], &stats[1]);
+		    
+		    msgleft = tmpleft - rank;
+		    msgright = tmpright + rank;
+	    } while(rank*10 != itag_r);
+    }
+	
+    MPI_Barrier(ring_comm);
+    end = MPI_Wtime();
+	
+    for(int i=0; i<size; i++){
+	    if (rank == i)
+		    printf("I am processor %d and i have received %d messages. My final messages have tag %d and value %d, %d\n", rank, size*2, itag_r, tmpleft, tmpright);
+    }
+	
+    MPI_Finalize( );
+   
+    if (rank == 0)
+	    printf("Runtime, %d, %f\n", size, end-start);
+	   
+    return 0;
+}
